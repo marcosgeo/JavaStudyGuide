8e1d1638102db4240775a6e45545ee3dfe6d85e0
Antonio Marcos <marcosgeo@yahoo.com>
2026-05-18 08:01:48 -0300

    compiling - the basics finished

---
+++ chp01/NumberPicker.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class NumberPicker {
+  public static void main(String[] args) {
+    Random r = new Random();
+    System.out.println(r.nextInt(10));  // a number between 0 and 9
+  }
+}
+   
